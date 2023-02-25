c981f7c6b269a51b41555f889b7d109bd6a7a711
Ammar Ahmed <ammar132004@gmail.com>
2023-02-25 12:29:07 +0200

    b

--- js/app.js
+++ js/app.js
@@ -110,16 +110,16 @@ async function getBotAnswers() {
 <div class="album-actions">
 <a href="#" class="album-action">
 <svg stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-<path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path></svg>5</a>
+<path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path></svg>0</a>
 <a href="#" class="album-action">
 <svg stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1" viewBox="0 0 24 24">
-<path d="M21 15a2 2 0 01-2 2H7l-4 4V5a2 2 0 012-2h14a2 2 0 012 2z"></path></svg>1</a>
+<path d="M21 15a2 2 0 01-2 2H7l-4 4V5a2 2 0 012-2h14a2 2 0 012 2z"></path></svg>0</a>
 <a href="#" class="album-action">
 <svg stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1" viewBox="0 0 24 24">
 <path d="M17 1l4 4-4 4"></path>
 <path d="M3 11V9a4 4 0 014-4h14M7 23l-4-4 4-4"></path>
 <path d="M21 13v2a4 4 0 01-4 4H3"></path>
-</svg>2</a>
+</svg>0</a>
 </div>
 </div>`;
     });
